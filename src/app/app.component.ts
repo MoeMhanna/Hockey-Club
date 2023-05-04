c850c51d9557bc2bcf35382de036ba954821b64b
Hamoud Mhanna <hamoud@alchemi-data.com>
2023-05-04 18:38:11 -0400

    add: mission add: academic add: teams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hocky-club';
+  public menuItems = [
+    {
+      label: 'Mission',
+      id: 'mission',
+      isSelected: true
+    },
+    {
+      label: 'Academic',
+      id: 'academic',
+      isSelected: false
+    },
+    {
+      label: 'Teams',
+      id: 'teams',
+      isSelected: false
+    },
+    {
+      label: 'Student Life',
+      id: 'student-life',
+      isSelected: false
+    },
+    {
+      label: 'Rink',
+      id: 'rink',
+      isSelected: false
+    },
+    {
+      label: 'Staff',
+      id: 'staff',
+      isSelected: false
+    },
+    {
+      label: 'Sponsors',
+      id: 'sponsors',
+      isSelected: false
+    },
+    {
+      label: 'Contact Us',
+      id: 'contact-us',
+      isSelected: false
+    }
+  ]
 }
